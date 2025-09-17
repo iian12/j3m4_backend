45429f776fd5687805e2c134d18155bd42fec527
ian <minchan3192@gmail.com>
2025-09-17 19:41:46 +0900

    0917 Start project

---
+++ src/main/java/com/example/j3m4_backend/J3m4BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.j3m4_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J3m4BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J3m4BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/j3m4_backend/J3m4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.j3m4_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J3m4BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
